1b17f719a7596a393396227c3bfabc5aeba6094b
idk20256 <emailmati98@gmail.com>
2025-05-19 07:20:27 +0700

    Update ping.py

--- WinxMusic/puglins/tools/ping.py
+++ WinxMusic/puglins/tools/ping.py
@@ -7,21 +7,20 @@ from WinxMusic.core.call import Winx
 from WinxMusic.utils import bot_sys_stats
 from WinxMusic.utils.decorators.language import language
 from WinxMusic.utils.inline import support_group_markup
-from config import BANNED_USERS, PING_IMG_URL
+from config import BANNED_USERS
+from config.assets import PINK_STICKER_ID #disnicuk
 from strings import command
-
 
 @app.on_message(command("PING_COMMAND") & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
-    response = await message.reply_photo(
-        photo=PING_IMG_URL,
-        caption=_["ping_1"].format(app.mention),
-    )
+    response = await message.reply_sticker(PING_STICKER_ID)
+
     start = datetime.now()
     pytgping = await Winx.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
+#kintil
     await response.edit_text(
         _["ping_2"].format(
             resp,
